77f58a67f04568c1f94d5b81dd52055d72181f46
James Schultz <ninjames101@gmail.com>
2019-01-12 15:04:57 -0700

    server is running, next is to make migrations and seeds

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5555
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res, next) => res.send('🦖'))
+
+app.listen(port, () => console.log(`got you on ${port}`))
